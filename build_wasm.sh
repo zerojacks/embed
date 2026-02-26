4d3973295df6e43e6f4301402e4317bc394e48a2
zerojackshi <zerojack.shi@gmail.com>
2026-02-26 11:29:41 +0800

    fix:修复编译脚本

--- build_wasm.sh
+++ build_wasm.sh
@@ -27,6 +27,6 @@ cd ../..
 
 # 删除 wasm-pack 生成的 .gitignore 文件，确保构建产物可以被提交
 echo "Removing pkg-web/.gitignore..."
-rm -f pkg-web/.gitignore
+rm -rf pkg-web/.gitignore
 
 echo "WASM build completed successfully!"
